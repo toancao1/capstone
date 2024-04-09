4662ec9195765a4fd4ccff45c5bcfc1d9fa8e521
Toan Cao <71416586+toancao1@users.noreply.github.com>
2024-04-09 01:23:39 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-/* https://www.w3schools.com/howto/howto_js_redirect_webpage.asp 
-https://www.w3schools.com/howto/howto_js_dropdown.asp, https://pagedart.com/blog/how-to-add-a-search-bar-in-html/ and https://javascript.plainenglish.io/how-to-build-a-search-bar-7d8a8a3d9d00*/
+// https://www.w3schools.com/howto/howto_js_redirect_webpage.asp 
+// https://www.w3schools.com/howto/howto_js_dropdown.asp
+// https://javascript.plainenglish.io/how-to-build-a-search-bar-7d8a8a3d9d00
 // https://stackoverflow.com/questions/503093/how-do-i-redirect-to-another-webpage
-// redirect to page
+// https://stackoverflow.com/questions/42043803/javascript-simple-search-how-to-display-result-on-new-page
+// https://www.w3schools.com/js/js_window_location.asp
+// https://medium.com/@ianflurkey/using-foreach-and-innerhtml-to-create-search-results-6b11b9985d6b 
+// https://www.w3schools.com/jsref/prop_node_textcontent.asp
+// Redirect to page
 function redirectTo(page) {
   window.location.href = page;
 }
@@ -14,9 +19,9 @@ function toggleDropdown() {
 
 // https://stackoverflow.com/questions/71726629/close-dropdown-menu-when-clicking-outside
 // https://github.com/sakshi1998/Bookart/blob/3ae179dca4b75de6372ada738773cd28c9f07e2b/bookart/login/satire/realistic/inde1.php
-// Close  dropdown menu if user clicks outside of it
 // https://www.outsystems.com/forums/discussion/70379/dropdown-with-javascript-and-css-wont-close-properly-when-clicking-outside-of-it/
 // https://dev.to/am20dipi/how-to-build-a-simple-search-bar-in-javascript-4onf
+// Close dropdown menu if user clicks outside of it
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
     var dropdowns =  document.getElementsByClassName("dropdown-content");
@@ -50,15 +55,9 @@ const pages = [
   { title: 'Videos', url: 'videos.html' },
 ];
 
-// https://stackoverflow.com/questions/42043803/javascript-simple-search-how-to-display-result-on-new-page
-// https://www.w3schools.com/js/js_window_location.asp
-// https://medium.com/@ianflurkey/using-foreach-and-innerhtml-to-create-search-results-6b11b9985d6b and https://forum.freecodecamp.org/t/adding-an-event-listener-to-dynamically-created-elements/185906/2
-// https://www.w3schools.com/jsref/prop_node_textcontent.asp
-
-
-// Function for displaying search results
 // https://stackoverflow.com/questions/63204750/addeventlistener-to-innerhtml
 // https://forum.freecodecamp.org/t/adding-an-event-listener-to-dynamically-created-elements/185906/2
+// Function for displaying search results
 function displayResults(results) {
   const searchResults = document.getElementById('searchResults');
   searchResults.innerHTML = '';
